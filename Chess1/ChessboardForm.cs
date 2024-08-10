9a9994af2bdb54306378b4d1b5bfcc204dff8115
Sagi Palachi <sagipalachi@gmail.com>
2024-08-10 17:44:29 +0300

    Init board with pieces

--- Chess1/ChessboardForm.cs
+++ Chess1/ChessboardForm.cs
@@ -1,17 +1,102 @@
-﻿using System;
+﻿using ChessBE;
+using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Net.NetworkInformation;
+using System.Reflection.Metadata.Ecma335;
 using System.Security.Policy;
 using System.Windows.Forms;
 
 public partial class ChessboardForm : Form
 {
     private Panel[,] _chessBoardPanels;
+    private Board board = new Board();
 
     public ChessboardForm()
     {
         InitializeWindow();
         InitializeChessboard();
+        DrawPieces();
+    }
+
+
+    private void DrawPieces()
+    {
+        List<Piece> pieces = board.GetPieces();
+        foreach (Piece piece in pieces)
+        {
+            DrawPiece(piece);
+        }
+    }
+
+    private void DrawPiece(Piece piece)
+    {
+        Image? image = GetImageForPiece(piece);
+        if (image != null)
+        {
+            Label label = new Label
+            {
+                Size = new Size(64,64),
+                Location = new Point(0,0),
+                Image = image,
+                //ImageAlign = ContentAlignment.TopCenter
+            };
+            _chessBoardPanels[piece.Pos.Col, piece.Pos.Row].Controls.Add(label);
+        }
+    }
+
+    private static Image? GetImageForPiece(Piece piece)
+    {
+        Image? res = null;
+        string fname = "";
+        if (piece.GetType() == typeof(King))
+        { fname = (piece.Color == PieceColor.White) ? "KingW.png" : "KingB.png"; }
+        if (piece.GetType() == typeof(Queen))
+        { fname = (piece.Color == PieceColor.White) ? "QueenW.png" : "QueenB.png"; }
+        if (piece.GetType() == typeof(Rook))
+        { fname = (piece.Color == PieceColor.White) ? "RookW.png" : "RookB.png"; }
+        if (piece.GetType() == typeof(Bishop))
+        { fname = (piece.Color == PieceColor.White) ? "BishopW.png" : "BishopB.png"; }
+        if (piece.GetType() == typeof(Knight))
+        { fname = (piece.Color == PieceColor.White) ? "KnightW.png" : "KnightB.png"; }
+        if (piece.GetType() == typeof(Pawn))
+        { fname = (piece.Color == PieceColor.White) ? "PawnW.png" : "PawnB.png"; }
+
+        if (fname.Length > 0) {
+            string fullpath = "G:\\c#\\Chess1\\Chess1\\ChessAssets\\" + fname;
+            Image img = Image.FromFile(fullpath);
+            res = ResizeImage(img, new Size(50, 50));
+        }
+        return res;
+    }
+
+    public static Image ResizeImage(Image imgToResize, Size size)
+    {
+        // Get the image's original width and height
+        int sourceWidth = imgToResize.Width;
+        int sourceHeight = imgToResize.Height;
+
+        // Calculate the new width and height
+        float nPercentW = (float)size.Width / sourceWidth;
+        float nPercentH = (float)size.Height / sourceHeight;
+        float nPercent = Math.Min(nPercentW, nPercentH);
+
+        int destWidth = (int)(sourceWidth * nPercent);
+        int destHeight = (int)(sourceHeight * nPercent);
+
+        // Create a new bitmap with the new dimensions
+        Bitmap b = new Bitmap(destWidth, destHeight);
+        Graphics g = Graphics.FromImage(b);
+
+        // Set the interpolation mode for high quality resizing
+        g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+
+        // Draw the resized image
+        g.DrawImage(imgToResize, 0, 0, destWidth, destHeight);
+        g.Dispose();
+
+        return b;
     }
 
     private void InitializeChessboard()
@@ -35,7 +120,7 @@ public partial class ChessboardForm : Form
                 _chessBoardPanels[row, col] = newPanel; // Store in our 2D array
 
                 // Alternate background colors
-                newPanel.BackColor = (row + col) % 2 == 0 ? Color.DarkBlue : Color.LightGray;
+                newPanel.BackColor = (row + col) % 2 == 0 ? Color.DarkGray : Color.DimGray;
             }
         }
     }
@@ -53,6 +138,24 @@ public partial class ChessboardForm : Form
     }
 
     private void ChessboardForm_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void InitializeComponent()
+    {
+            this.SuspendLayout();
+            // 
+            // ChessboardForm
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "ChessboardForm";
+            this.Load += new System.EventHandler(this.ChessboardForm_Load_1);
+            this.ResumeLayout(false);
+
+    }
+
+    private void ChessboardForm_Load_1(object sender, EventArgs e)
     {
 
     }
---
+++ ChessBE/Pieces.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBE
+{
+    public abstract class Piece
+    {
+        public Position? Pos { get; set; }
+        public PieceColor Color = PieceColor.Black;
+
+        public Piece(Position? pos, PieceColor color)
+        {
+            this.Pos = pos;
+            this.Color = color;
+        }
+    }
+
+    public class Position
+    {
+        public int Row { get; set; }
+        public int Col { get; set; }
+
+        public Position(int row, int col)
+        {
+            Row = row;
+            Col = col;
+        }
+    }
+
+    public enum PieceColor
+    {
+        White,
+        Black,
+    }
+
+   
+    public class King : Piece
+    {
+        public King(Position? pos, PieceColor color) : base(pos, color)
+        {
+        }
+    }
+    public class Queen : Piece
+    {
+        public Queen(Position? pos, PieceColor color) : base(pos, color)
+        {
+        }
+    }
+    public class Rook : Piece
+    {
+        public Rook(Position? pos, PieceColor color) : base(pos, color)
+        {
+        }
+    }
+    public class Knight : Piece
+    {
+        public Knight(Position? pos, PieceColor color) : base(pos, color)
+        {
+        }
+    }
+    public class Bishop : Piece
+    {
+        public Bishop(Position? pos, PieceColor color) : base(pos, color)
+        {
+        }
+    }
+    public class Pawn : Piece
+    {
+        public Pawn(Position? pos, PieceColor color) : base(pos, color)
+        {
+        }
+    }
+
+}
