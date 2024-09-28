803cea80e45ac7ff1e4e2bdd95f0fdc550491a24
Sagi Palachi <sagipalachi@gmail.com>
2024-09-28 18:47:43 +0300

    Queen movement

--- ChessBE/Pieces/Queen.cs
+++ ChessBE/Pieces/Queen.cs
@@ -11,6 +11,76 @@ namespace ChessBE.Pieces
         public Queen(Position? pos, PieceColor color) : base(pos, color)
         {
         }
+        public override List<Position>? GetPotentialPositions()
+        {
+            if (Pos == null)
+                return null;
+            List<Position>? positions = new List<Position>();
+            for (int i = Pos.Row + 1; i < 8; i++)
+            {
+                Position pRow = new Position(Pos.Col, i);
+                if (!addToPositions(pRow, positions))
+                    break;
+            }
+            for (int i = Pos.Row - 1; i >= 0; i--)
+            {
+                Position pRow = new Position(Pos.Col, i);
+                if (!addToPositions(pRow, positions))
+                    break;
+            }
+            for (int i = Pos.Col + 1; i < 8; i++)
+            {
+                Position pCol = new Position(i, Pos.Row);
+                if (!addToPositions(pCol, positions))
+                    break;
+            }
+            for (int i = Pos.Col - 1; i >= 0; i--)
+            {
+                Position pCol = new Position(i, Pos.Row);
+                if (!addToPositions(pCol, positions))
+                    break;
+            }
+            for (int i = 1; i < 8; i++)
+            {
+                Position p1 = new Position(Pos.Col + i, Pos.Row + i);
+                if (!addToPositions(p1, positions))
+                    break;
+            }
+            for (int i = 1; i < 8; i++)
+            {
+                Position p2 = new Position(Pos.Col + i, Pos.Row - i);
+                if (!addToPositions(p2, positions))
+                    break;
+            }
+            for (int i = 1; i < 8; i++)
+            {
+                Position p3 = new Position(Pos.Col - i, Pos.Row + i);
+                if (!addToPositions(p3, positions))
+                    break;
+            }
+            for (int i = 1; i < 8; i++)
+            {
+                Position p4 = new Position(Pos.Col - i, Pos.Row - i);
+                if (!addToPositions(p4, positions))
+                    break;
+            }
+            return positions;
+        }
 
+        private bool addToPositions(Position pos, List<Position> posList)
+        {
+            Piece otherPiece = Board.GetInstance().Occupied(pos);
+            if (otherPiece == null)
+            {
+                pos.AddToList(posList);
+                return true;
+            }
+            else if (IsEnemy(otherPiece))
+            {
+                pos.AddToList(posList);
+                return false;
+            }
+            return false;
+        }
     }
 }
