b0bd38c0e3a3151144a4b2fd307bceb9acb2c55d
Sagi Palachi <sagipalachi@gmail.com>
2024-09-28 18:18:51 +0300

    Rooks movment

--- ChessBE/Pieces/Rook.cs
+++ ChessBE/Pieces/Rook.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,54 @@ namespace ChessBE.Pieces
         public Rook(Position? pos, PieceColor color) : base(pos, color)
         {
         }
+        public override List<Position>? GetPotentialPositions()
+        {
+            if (Pos == null)
+                return null;
+            List<Position>? positions = new List<Position>();
+            for (int i = Pos.Row + 1; i < 8; i++)
+            {
+                Position pRow = new Position(Pos.Col, i);
+                if (!addToPositions(pRow, positions))
+                    break;
+            }
+            for (int i = Pos.Row - 1; i >= 0; i--)
+            {
+                Position pRow = new Position(Pos.Col, i);
+                if (!addToPositions(pRow, positions))
+                    break;
+            }
+            for (int i = Pos.Col+1; i < 8; i++)
+            {
+                Position pCol = new Position(i, Pos.Row);
+                if (!addToPositions(pCol, positions))
+                    break;
+            }
+            for (int i = Pos.Col - 1; i >= 0; i--)
+            {
+                Position pCol = new Position(i, Pos.Row);
+                if (!addToPositions(pCol, positions))
+                    break;
+            }
+            return positions;
+        }
 
+        private bool addToPositions(Position pos, List<Position> posList)
+        {
+            Piece otherPiece = Board.GetInstance().Occupied(pos);
+            if (otherPiece == null)
+            {
+                pos.AddToList(posList);
+                return true;
+            }
+            else if (IsEnemy(otherPiece))
+            {
+                pos.AddToList(posList);
+                return false;
+            }
+            return false;
+        }
     }
-
 }
+
+
